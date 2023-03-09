4da90aa27661c66daf57af9e49b1e8d6820016d7
batumunwa <batumunwa@gmail.com>
2023-03-09 18:39:24 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [taskName, setTaskName] = useState("")
+  const [tasks, setTaks] = useState([])
+  const taskArray = []
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Tasks</h1>
+      Add New Task
+      <input type="text"
+      value={taskName}
+      onChange={(e)=>setTaskName(e.target.value)}
+      />
+      <button onClick={() => {
+        if(taskName != ""){
+        setTaskName("")
+        tasks.push(taskName)
+        }
+      }}>Add</button>
+ 
+      <table width="100%">
+        <tr>
+          <td align='center'>
+            <table width="18%">
+                  {tasks.map(
+                  (task)=>(
+                    <tr>
+                      <td width="10"><input type="checkbox"/></td>
+                      <td>{task}</td>
+                    </tr>
+                    )
+                  )}
+             </table>
+            </td>
+        </tr>
+      </table>
+      
+        
     </div>
   );
 }
